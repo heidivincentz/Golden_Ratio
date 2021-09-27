bb098ffb63827fe3ac7a3c11ea400b989943bece
Heidi Vincentz <heid344m@stud.kea.dk>
2021-09-27 22:10:43 +0200

    database connected

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
-const url = "https://keadata-ece4.restdb.io";
+const url = "https://keadata-ece4.restdb.io/rest/golden-ratio-database";
 
 // API key
-const options = { header: { "x-apikey": "6151ddb6dfa7346e2f9690b9" } };
+const options = { headers: { "x-apikey": "6151ddb6dfa7346e2f9690b9" } };
+
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+
+  .then((data) => {
+    // console.log(data);
+  })
+
+  .catch((e) => {
+    console.error("An error occured", e.message);
+  });
+
+function handleData(artpiece) {
+  superheroes.forEach((piece) => {
+    // console.log(piece);
+    // Create template
+    // Grab
+    // Clone
+    // Populate
+    // Append
+  });
+}
