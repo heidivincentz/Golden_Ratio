11b3b75ba833a365b914b5329388b231f00cf212
zinasemia <89393314+zinasemia@users.noreply.github.com>
2021-09-28 16:32:54 +0200

    addnewurl

--- js/script.js
+++ js/script.js
@@ -6,26 +6,6 @@ const url = "https://keadata-ece4.restdb.io/rest/golden-ratio-database";
 // API key
 const key = { headers: { "x-apikey": "6151ddb6dfa7346e2f9690b9" } };
 
-//Heidi's changes
-/* fetch(url, key)
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-
-  .then(function (data) {
-    // console.log(data);
-    handleData(data);
-
-  })
-
-
-  .catch((e) => {
-    console.error("An error occured", e.message);
-  });
- */
 
 fetch(url, key)
   .then(function (res) {
@@ -43,10 +23,8 @@ function handleData(data) {
 function showData(obj) {
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
-  clone.querySelector("h1.listh1").textContent = obj.title;
   clone.querySelector("h2").textContent = obj.artist;
   clone.querySelector(".country span").textContent = obj.country;
-
   const parent = document.querySelector("main");
   parent.appendChild(clone);
 }
