9820ba2a7ee351becfd87ddbb8754a98216f2b98
zinasemia <89393314+zinasemia@users.noreply.github.com>
2021-09-28 15:53:02 +0200

    addconfusion

--- artist.js
+++ artist.js
@@ -4,29 +4,34 @@ const artist = urlParams.get("artist");
 const url = "https://keadata-ece4.restdb.io/rest/golden-ratio-database";
 
 // API key
-const key = { headers: { "x-apikey": "6151ddb6dfa7346e2f9690b9" } };
+const key = {
+    headers: {
+        "x-apikey": "6151ddb6dfa7346e2f9690b9"
+    }
+};
 
 
 fetch(url, key)
-  .then(function (res) {
-    return res.json();
-  })
+    .then(function (res) {
+        return res.json();
+    })
 
-  .then(function (data) {
-    handleData(data);
-  });
+    .then(function (data) {
+        handleData(data);
+    });
 
 function handleData(data) {
-  data.forEach(showData);
+    data.forEach(showData);
 }
 
-function showData(obj) {console.log("data");
-  const template = document.querySelector("template").content;
-  const clone = template.cloneNode(true);
-  clone.querySelector(".headline").textContent = obj.artist;
-  clone.querySelector(".aside").textContent = obj.bio;
-  clone.querySelector(".imgforgrid").src = obj.img;
+function showData(obj) {
+    console.log("data");
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+    clone.querySelector(".headline").textContent = obj.artist;
+    clone.querySelector(".aside").textContent = obj.bio;
+    clone.querySelector(".imgforgrid").src = `https://neurotive.com/KEA/Golden_Ratio/louisiana_archivephotos_golden-ratio/fraTMS_3077.jpg`;
 
-  const parent = document.querySelector("main");
-  parent.appendChild(clone);
+    const parent = document.querySelector("main");
+    parent.appendChild(clone);
 }
